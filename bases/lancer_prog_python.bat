8558ed53fdd61ab0887f67569662697a99bfb6bf
didier <didier@d-defrance.fr>
2020-05-06 19:52:30 +0200

    renamed

--- bases/lancer_prog_python.bat
+++ bases/lancer_prog_python.bat
@@ -1,3 +1,17 @@
-set PYTHON_HOME=C:\ProgramData\Anaconda3
-%PYTHON_HOME%/python p1.py
+REM avec PATH contenant le répertoire d'installation de python
+REM python hello.py
+REM python p1_print_input.py
+REM python p2_type_if_else_for.py
+REM python p3_while_liste.py
+REM python p4_def_function.py
+REM python my_app.py
+REM python p5_math_random.py
+REM python listes.py
+REM python ensembles.py
+REM python dictionnaires.py
+REM python dates.py
+REM python strings.py
+REM python exceptions.py
+REM python fichiers.py
+python objets.py
 pause
--- with-conda/lancer_prog_python.bat
+++
@@ -1,6 +0,0 @@
-REM avec PATH contenant le répertoire d'installation de python
-set PYTHON_HOME=C:\ProgramData\Anaconda3
-%PYTHON_HOME%/python graph.py
-REM python graph.py
-
-pause
