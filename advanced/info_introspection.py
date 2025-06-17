cbfd66f998b5ad5e644363b8d712c8d414776897
Didier_Defrance <didier@d-defrance.fr>
2025-06-17 18:26:37 +0200

    s

---
+++ advanced/info_introspection.py
@@ -0,0 +1,7 @@
+from apihelper import info
+li = []
+info(li)
+
+import inspect
+print(inspect.getmembers(str))
+# Output: [('__add__', <slot wrapper '__add__' of ... ...
--- bases/my_util/op3.py
+++ bases/my_util/op3.py
@@ -1,4 +1,5 @@
 def tripleDe(x):
+	print(f"tripleDe(), module_name: __name__ = {__name__}") # my_util.op3
 	return 3*x
 	
 def tiersDe(x):
